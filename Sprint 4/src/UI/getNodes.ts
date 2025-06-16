c9fa8d8386c26728f8b0b9d9c23797be63e123a9
mstne03 <mserraitorne@gmail.com>
2025-06-16 13:08:23 +0200

    Gets all DOM nodes necessary to interact with the user.

---
+++ Sprint 4/src/UI/getNodes.ts
@@ -0,0 +1,3 @@
+export const jokeButton : HTMLButtonElement = document.querySelector('.header_btn')!;
+export const jokeOutput : HTMLDivElement = document.querySelector('#joke')!;
+export const scoreButton : NodeListOf<HTMLButtonElement> = document.querySelectorAll('.score_btn')!;
