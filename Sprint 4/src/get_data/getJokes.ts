40afb3b73d04c4263ba6108ccfd8cae5c7451e61
mstne03 <mserraitorne@gmail.com>
2025-06-16 12:57:40 +0200

    Creates getJokes function. Its purpose is to return a joke from the icanhazdadjoke.com api.

---
+++ Sprint 4/src/get_data/getJokes.ts
@@ -0,0 +1,25 @@
+import type { JokeJson, err } from '../logic/interfaces.ts'
+
+export default async function getJokes () : Promise<string> {
+    let jokeUrl : string = 'https://icanhazdadjoke.com/';
+
+    try {
+        let response : Response = await fetch(jokeUrl, {
+            method: 'GET',
+            headers: {
+            'Accept': 'application/json'
+            }
+        });
+
+        if (!response.ok) 
+            throw new Error(`HTTP error! status: ${response.status}`);
+
+        let jsonJoke : JokeJson = await response.json();
+
+        return jsonJoke.joke;
+    }
+    
+    catch (error) {
+        return `Error ${(error as err).name}: ${(error as err).message}`;
+    }
+}
